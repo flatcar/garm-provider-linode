6a47194e0e4da61424d07392cece74478bae6f8d
Mathieu Tortuyaux <mtortuyaux@microsoft.com>
2025-06-04 17:53:28 +0200

    client: create linodego client
    Signed-off-by: Mathieu Tortuyaux <mtortuyaux@microsoft.com>

--- client/client.go
+++ client/client.go
@@ -2,12 +2,43 @@
 
 package client
 
-import "github.com/flatcar/garm-provider-linode/config"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-type LinodeClient struct{}
+	"golang.org/x/oauth2"
+
+	"github.com/linode/linodego"
+
+	"github.com/flatcar/garm-provider-linode/config"
+)
+
+type LinodeClient struct {
+	Client *linodego.Client
+}
 
 // New returns a new Linode client.
 func New(cfg *config.Config, controllerID string) (*LinodeClient, error) {
-	return &LinodeClient{}, nil
+	if cfg == nil {
+		return nil, fmt.Errorf("configuration is nil")
+	}
+
+	if err := cfg.Validate(); err != nil {
+		return nil, fmt.Errorf("validating configuration: %w", err)
+	}
+
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: cfg.Token})
+	oauth2Client := &http.Client{
+		Transport: &oauth2.Transport{
+			Source: tokenSource,
+		},
+	}
+
+	client := linodego.NewClient(oauth2Client)
+
+	return &LinodeClient{Client: &client}, nil
+}
 
 }
