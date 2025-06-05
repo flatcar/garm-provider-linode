f3b9746538f68687695d7ed9380891435ecb3d76
Mathieu Tortuyaux <mtortuyaux@microsoft.com>
2025-06-05 10:46:56 +0200

    provider: add instance status and ipv4
    Signed-off-by: Mathieu Tortuyaux <mtortuyaux@microsoft.com>

--- provider/provider.go
+++ provider/provider.go
@@ -9,14 +9,30 @@ import (
 
 	"github.com/flatcar/garm-provider-linode/client"
 	"github.com/flatcar/garm-provider-linode/config"
+	"github.com/linode/linodego"
 
 	execution "github.com/cloudbase/garm-provider-common/execution/v0.1.0"
 	"github.com/cloudbase/garm-provider-common/params"
 )
 
-var _ execution.ExternalProvider = &linodeProvider{}
-
-var Version = "v0.0.0-unknown"
+var (
+	_       execution.ExternalProvider = &linodeProvider{}
+	Version                            = "v0.0.0-unknown"
+	status                             = map[linodego.InstanceStatus]params.InstanceStatus{
+		linodego.InstanceRunning:      params.InstanceRunning,
+		linodego.InstanceOffline:      params.InstanceStopped,
+		linodego.InstanceDeleting:     params.InstanceDeleting,
+		linodego.InstanceProvisioning: params.InstancePendingCreate,
+		linodego.InstanceBooting:      params.InstanceCreating,
+		linodego.InstanceShuttingDown: params.InstanceStopped,
+		linodego.InstanceRebooting:    params.InstanceStatusUnknown,
+		linodego.InstanceMigrating:    params.InstanceStatusUnknown,
+		linodego.InstanceRebuilding:   params.InstanceStatusUnknown,
+		linodego.InstanceCloning:      params.InstanceStatusUnknown,
+		linodego.InstanceRestoring:    params.InstanceStatusUnknown,
+		linodego.InstanceResizing:     params.InstanceStatusUnknown,
+	}
+)
 
 type linodeProvider struct {
 	cfg          *config.Config
@@ -66,10 +82,31 @@ func (p *linodeProvider) ListInstances(ctx context.Context, poolID string) ([]pa
 
 	res := make([]params.ProviderInstance, len(instances))
 	for i, instance := range instances {
-		res[i] = params.ProviderInstance{
+		// Extra safety net if the instance status does not exist.
+		instanceStatus := params.InstanceStatusUnknown
+		if s, ok := status[instance.Status]; ok {
+			instanceStatus = s
+		}
+
+		inst := params.ProviderInstance{
 			ProviderID: strconv.Itoa(instance.ID),
 			Name:       instance.Label,
+			Status:     instanceStatus,
+			// TODO: Add OSType, OSName, OSVersion and OSArch.
 		}
+
+		// Best effort to get the public IP.
+		ipv4s := instance.IPv4
+		if len(ipv4s) > 0 {
+			inst.Addresses = []params.Address{
+				{
+					Type:    params.PublicAddress,
+					Address: ipv4s[0].String(),
+				},
+			}
+		}
+
+		res[i] = inst
 	}
 
 	return res, nil
